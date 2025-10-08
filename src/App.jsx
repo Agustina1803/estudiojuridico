f26d43be40d15b279c777bb070e4def0d6e4c964
ANCoronel <adrian.coronel86@hotmail.com>
2025-10-08 09:35:31 -0300

    Prueba solucion error

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { RegistroPage } from './components/RegistroPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
   return (
     <>
-      <RegistroPage/>
+     
     </>
   );
 }
