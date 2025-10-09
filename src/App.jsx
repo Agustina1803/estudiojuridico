63cfa1d4b8f71f70c8e50751efb480404ef3306c
AgusBulacio <agusbulacio1803@gmail.com>
2025-10-08 23:28:09 -0300

    modifique el app

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,12 @@
-import { useState } from "react";
-import { Button } from "react-bootstrap";
-import FormNuevaTarea from "./components/FormNuevaTarea";
-import FormNuevoCliente from "./components/FormNuevoCliente";
 import "./App.css";
 
 
 
 function App() {
-  const [mostrarTarea, setMostrarTarea] = useState(false);
-  const [mostrarCliente, setMostrarCliente] = useState(false);
+
 
   return (
-    <div className="container mt-4">
-      <h1>Gestión de Tareas</h1>
-
-      <div className="d-flex gap-2">
-        <Button variant="primary" onClick={() => setMostrarTarea(true)}>
-          Agregar Tarea
-        </Button>
-        <Button variant="success" onClick={() => setMostrarCliente(true)}>
-          Agregar Cliente
-        </Button>
-      </div>
-
-      <FormNuevaTarea
-        mostrar={mostrarTarea}
-        cerrar={() => setMostrarTarea(false)}
-      />
-
-      <FormNuevoCliente
-        mostrar={mostrarCliente}
-        cerrar={() => setMostrarCliente(false)}
-      />
-    </div>
+   <></>
   );
 }
 export default App;
