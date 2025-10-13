b815c57f9fac9a9c6c3efa57b0b07678c5148212
AgusBulacio <agusbulacio1803@gmail.com>
2025-10-13 13:44:36 -0300

    acomode los contenedores del app del div y el main para que acomode la posicion de los elementos del navBar, Footer y el formLogeo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route,} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import './App.css';
+import Footer from "./shared/footer.jsx";
 import { LoginPages } from './pages/LoginPages'
 import { AdminPages } from './pages/AdminPages'
 import { AbogPages } from './pages/AbogPages'
@@ -8,21 +9,24 @@ import { SecrePages } from './pages/SecrePages'
 import { ErrorPages } from './pages/ErrorPages'
 
 
+
 function App() {
-
-
   return (
-    
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LoginPages />} />
-        <Route path='/admin' element={<AdminPages />} />
-        <Route path='/abog' element={<AbogPages />} />
-        <Route path='/secre' element={<SecrePages />} />
-        <Route path='*' element={<ErrorPages />} />
-      </Routes>
+      <div className="app-layout">
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<LoginPages />} />
+            <Route path="/admin" element={<AdminPages />} />
+            <Route path="/abog" element={<AbogPages />} />
+            <Route path="/secre" element={<SecrePages />} />
+            <Route path="*" element={<ErrorPages />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
-  )
-};
+  );
+}
 
 export default App;
