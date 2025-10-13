b595176f7478779b46e52b32cc676c66978ffaf3
AgusBulacio <agusbulacio1803@gmail.com>
2025-10-13 13:35:26 -0300

    Agrege el texto del footer y le di estilo al mismo

---
+++ src/shared/Footer.jsx
@@ -0,0 +1,11 @@
+import '../styles/footer.css'
+
+const Footer = () => {
+  return (
+ <footer className="backgraundFooter w-100">
+  <p className="mb-0">© Todos los derechos reservados</p>
+</footer>
+  )
+}
+
+export default Footer
