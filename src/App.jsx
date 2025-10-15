c87c6762a8aa6a17d9fdd1581fc7fbc76bd1a863
AgusBulacio <agusbulacio1803@gmail.com>
2025-10-15 15:40:39 -0300

    creee el archivo Proteccion de rutas para proteger las rutas del login

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,27 @@ import { AbogPages } from './pages/AbogPages'
 import { SecrePages } from './pages/SecrePages'
 import  ErrorPages  from './pages/ErrorPages'
 import { useState } from 'react';
+import ProteccionRutas from './routers/ProteccionRutas.jsx';
 
 function App() {
 const usuarioSessionStorage = JSON.parse(sessionStorage.getItem('userKey')) || false
 const [usuariorLogeado, setUsuarioLogeado] = useState(usuarioSessionStorage)
 
   return (
-      
     <BrowserRouter>
       <div className="app-layout">
         <main className="main-content">
           <Routes>
             <Route path="/" element={<LoginPages setUsuarioLogeado={setUsuarioLogeado} />} />
-            <Route path="/admin" element={<AdminPages />} />
-            <Route path="/abog" element={<AbogPages />} />
-            <Route path="/secre" element={<SecrePages />} />
+            <Route path="/admin" element={<ProteccionRutas setUsuarioLogeado={setUsuarioLogeado} ></ProteccionRutas>}>
+            <Route index element={<AdminPages/>}></Route>
+            </Route>
+            <Route path="/abog" element={<ProteccionRutas setUsuarioLogeado={setUsuarioLogeado} ></ProteccionRutas>}>
+            <Route index element={<AbogPages/>}></Route>
+            </Route>
+             <Route path="/secre" element={<ProteccionRutas setUsuarioLogeado={setUsuarioLogeado} ></ProteccionRutas>}>
+            <Route index element={<SecrePages/>}></Route>
+            </Route>
             <Route path="*" element={<ErrorPages />} />
           </Routes>
         </main>
