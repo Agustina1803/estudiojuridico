59a132fb02e880a472428eaf9177ee9237e21e74
AgusBulacio <agusbulacio1803@gmail.com>
2025-10-15 13:57:42 -0300

    corregi el codigo de la pagina erroPages ya que estaba siendo llamado fuera del return, y en la pagina registro llame al form para las validaciones

--- src/components/RegistroPage.jsx
+++ src/components/RegistroPage.jsx
@@ -1,21 +1,49 @@
-// src/assets/Components/RegistroPage.jsx
-import { FloatingLabel, Form, Button, Card } from 'react-bootstrap';
+import {Form, Button, Card } from 'react-bootstrap';
 import '../styles/RegistroPage.css';
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
+//ver como cambiar el header despues del loguin por el del boton de cerrar sesion.
 export function RegistroPage() {
-  const {
+const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
   } = useForm();
+
+  const navegacion = useNavigate();
+const loginUser = (user) => {
+  const { formBasicEmail, formBasicPassword } = user;
+
+  if (
+    formBasicEmail === import.meta.env.VITE_ADMIN_EMAIL &&
+    formBasicPassword === import.meta.env.VITE_ADMIN_PASSWORD
+  ) {
+    navegacion("/admin");
+  } else if (
+    formBasicEmail === import.meta.env.VITE_SECRE_EMAIL &&
+    formBasicPassword === import.meta.env.VITE_SECRE_PASSWORD
+  ) {
+    navegacion("/secre");
+  } else if (
+    formBasicEmail === import.meta.env.VITE_ABOG_EMAIL &&
+    formBasicPassword === import.meta.env.VITE_ABOG_PASSWORD
+  ) {
+    navegacion("/abog");
+  } else {
+    alert("Usuario o contraseña incorrectos");
+    reset();
+  }
+};
+
+//Cambiar los nombre de los controles por FormBasicEmail y FormBasicPassword
   return (
     <Card border="primary" className='FormRegistro'>
       <div>
         <h3 className='text-center'>Iniciar Sesión</h3>
       </div>
-      <Form onSubmit={handleSubmit()}>
+      <Form onSubmit={handleSubmit( loginUser)}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email</Form.Label>
           <Form.Control type="email" placeholder="Enter email" {...register("formBasicEmail", {
@@ -26,7 +54,7 @@ export function RegistroPage() {
             }
           })} />
           <Form.Text className="text-danger">
-            {errors.Email?.message}
+            {errors.formBasicEmail?.message}
           </Form.Text>
         </Form.Group>
 
@@ -35,13 +63,15 @@ export function RegistroPage() {
           <Form.Control type="password" placeholder="Contraseña" 
           {...register("formBasicPassword", {
             required: "La contraseña es obligatorio",
-            pattern: {
-              value: /^(?=.\d)(?=.[\u0021-\u002b\u003c-\u0040])(?=.[A-Z])(?=.[a-z])\S{8,16}$/,
-              message: "La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter no alfanumérico",
-            }
+          pattern: {
+                    value:
+                      /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/,
+                    message:
+                      "La contraseña debe tener entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter especial.",
+                  }
           })} />
           <Form.Text className="text-danger">
-            {errors.Password?.message}
+            {errors.formBasicPassword?.message}
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
--- src/pages/LoginPages.jsx
+++ src/pages/LoginPages.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { RegistroPage } from "../components/RegistroPage"
 
 export const LoginPages = () => {
-  return (
 
-    <div>LoginPages</div>
+  
+
+  return (
+<>
+<div className="container m-3">
+<RegistroPage />
+</div>
+
+
+</>
 
   )
 }
