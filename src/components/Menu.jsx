de93a7e000705daf927366487ed32a783fd68fb9
daiana varela <dai.varela01@gmail.com>
2025-10-17 11:19:37 -0300

    se realizo el componente menu con sus rutas e iconos

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,69 @@
 import React from 'react'
+import {
+    FaHome,
+    FaUser,
+    FaBalanceScale,
+    FaUsers,
+    FaFileAlt,
+    FaChartBar,
+    FaCog,
+    FaCalendarAlt,
+    FaTasks,
+    FaMoneyBillWave,
+    FaFolder,
+    MdCheckCircle,
+} from 'react-icons/fa';
 
-export const Menu = () => {
-  return (
-    <div>Menu</div>
-  )
+const Menu = ({ role }) => {
+    const menus = () => {
+        switch (role) {
+            case 'admin':
+                return [
+                    { to: '/administrador', label: 'Inicio', icon: <FaHome /> },
+                    { to: '/administrador/usuariosadmi', label: 'Usuarios', icon: <FaUsers /> },
+                    { to: '/administrador/abogadosadmi', label: 'Abogados', icon: < FaBalanceScale /> },
+                    { to: '/administrador/secretarioadmi', label: 'Clientes', icon: <FaFileAlt /> },
+                    { to: '/administrador/documentosadmi', label: 'Documentos', icon: <FaFolder /> },
+                    { to: '/administrador/reportes', label: 'Reportes', icon: <FaChartBar /> },
+                    { to: '/administrador/configuracionadmi', label: 'Configuración', icon: <FaCog /> },
+                ];
+            case 'abog':
+                return [
+                    { to: '/abogado', label: 'Inicio', icon: <FaHome /> },
+                    { to: '/abogado/agendaabog', label: 'Agenda', icon: <FaCalendarAlt /> },
+                    { to: '/abogado/clienteabog', label: 'Cliente', icon: <FaUsers /> },
+                    { to: '/abogado/documentoabog', label: 'Documento', icon: <FaFolder /> },
+                    { to: '/abogado/tareasabog', label: 'Tareas', icon: <MdCheckCircle /> },
+                    { to: '/abogado/facturacionabog', label: 'Facturación', icon: <FaMoneyBillWave /> },
+                    { to: '/abogado/informeabog', label: 'Informe', icon: <FaChartBar /> },
+                    { to: '/abogado/juiciosabog', label: 'Juicios', icon: <FaBalanceScale /> },
+                ];
+            case 'secre':
+                return [
+                  { to: '/secretario', label: 'Inicio', icon: <FaHome /> },
+                    { to: '/secretario/agendasecre', label: 'Agenda', icon: <FaCalendarAlt /> },
+                    { to: '/secretario/clientesecre', label: 'Cliente', icon: <FaUser /> },
+                    { to: '/secretario/documentossecre', label: 'Documentos', icon: <FaFolder /> },
+                    { to: '/secretario/tareassecre', label: 'Tareas', icon: <MdCheckCircle/> },
+                    { to: '/secretario/facturacionsecre', label: 'Facturación', icon: <FaMoneyBillWave /> },
+                    { to: '/secretario/informessecre', label: 'Informes', icon: <FaChartBar /> },
+                ];
+            default:
+                return [];
+        }
+    }
+    return (
+         <Card>
+      <Card.Header>Featured</Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    )
+
 }
+export default Menu;
