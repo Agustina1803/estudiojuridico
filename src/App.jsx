94c67e5cb51e7149f9dc1b485757561991ca8699
daiana varela <dai.varela01@gmail.com>
2025-10-18 16:36:19 -0300

    volvi al archivo app a como estaba originalmente

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
+import { BrowserRouter, Routes, Route,} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import Menu from './components/Menu';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import { LoginPages } from './pages/LoginPages'
+import { AdminPages } from './pages/AdminPages'
+import { AbogPages } from './pages/AbogPages'
+import { SecrePages } from './pages/SecrePages'
+import { ErrorPages } from './pages/ErrorPages'
 
 
 
 
 function App() {
-
+const usuarioSessionStorage = JSON.parse(sessionStorage.getItem('userKey')) || false
+const [usuariorLogeado, setUsuarioLogeado] = useState(usuarioSessionStorage)
 
   return (
     <BrowserRouter>
-    
- <div>
-  <Routes>
-   <Route path="/" element={<Menu role="admin"/>}/>
-
-  </Routes>
-   </div>
-
+      <Routes>
+        <Route path='/' element={<LoginPages />} />
+        <Route path='/admin' element={<AdminPages />} />
+        <Route path='/abog' element={<AbogPages />} />
+        <Route path='/secre' element={<SecrePages />} />
+        <Route path='*' element={<ErrorPages />} />
+      </Routes>
     </BrowserRouter>
-    
   )
 };
 
