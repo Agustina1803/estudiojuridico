554e3e1fe03ba62a3a375354ba0f07f09ef3e720
AgusBulacio <agusbulacio1803@gmail.com>
2025-10-19 23:30:45 -0300

    corregi el tamaño de las LayoutsUser

--- src/layouts/LayoutsUser.jsx
+++ src/layouts/LayoutsUser.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Menu from "../components/Menu";
 import Footer from "../shared/footer";
@@ -14,13 +13,13 @@ const LayoutsAdmi = ({usuariorLogeado}) => {
       <main className="flex-grow-1">
         <Container fluid className="my-4">
           <Row>
-            <Col md={3} className="d-flex align-items-center justify-content-center">
-              <Menu role={usuariorLogeado?.role}/>
+            <Col md={3} lg={3} className="d-flex align-items-center justify-content-center">
+              <Menu role={usuariorLogeado?.role} className="menu"/>
             </Col>
-            <Col md={6}>
+            <Col md={6} lg={6}>
               <Outlet></Outlet>
             </Col>
-            <Col md={3}>
+            <Col md={3} lg={3}>
               <ChatInterno></ChatInterno>
             </Col> 
           </Row>
