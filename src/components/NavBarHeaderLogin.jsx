09b992ec99d38d7e8198250d9d15cbc1bd9fb767
stecc88 <steccoh88@gmail.com>
2025-10-23 10:33:54 -0300

    Puse logo

--- src/components/NavBarHeaderLogin.jsx
+++ src/components/NavBarHeaderLogin.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { Navbar, Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../assets/logo.jpg"; 
+import '../styles/navBarHeader.css';
 
 const NavBarHeaderLogin = () => {
   return (
-    <Navbar      
-      style={{ backgroundColor: "#1E3A5F" }}
+    <Navbar
       
+      className="styloHeader"
     >
       <Container fluid className="d-flex justify-content-start align-items-center">
-       
-        <Navbar.Brand className="text-white fw-bold fs-4 ms-3">
-          MI ESTUDIO
+        
+        <Navbar.Brand className="ms-3">
+          <img
+            src={logo}
+            alt="Logo del estudio"
+            height="45"
+            className="d-inline-block align-top"
+          />
         </Navbar.Brand>
       </Container>
     </Navbar>
