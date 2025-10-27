7d267ddd59ac842dea5b68543fec3051b5930f5f
AgusBulacio <agusbulacio1803@gmail.com>
2025-10-27 08:57:29 -0300

    agrege cambios

--- src/pages/Secretario/AgendaSecre.jsx
+++ src/pages/Secretario/AgendaSecre.jsx
@@ -46,7 +46,7 @@ const AgendaSecre = () => {
   const eliminar = (id) => {
     const cliente = filas.find((item) => item.id === id);
     Swal.fire({
-      title: `¿Eliminar la ${cliente.tipoEvento} de ${cliente.cliente}?`,
+      title: `¿Eliminar la ${cliente.tipoEvento} del cliente ${cliente.cliente}?`,
       text: "Este cambio no se puede revertir",
       icon: "warning",
       showCancelButton: true,
@@ -61,7 +61,7 @@ const AgendaSecre = () => {
         localStorage.setItem(tipo, JSON.stringify(actualizadas));
         Swal.fire({
           title: "Eliminado",
-          text: "La cita fue eliminada correctamente.",
+          text: "La cita  fue eliminada correctamente.",
           icon: "success",
         });
       }
