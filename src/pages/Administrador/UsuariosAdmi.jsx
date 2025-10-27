a84bdd4db7ae791e41d9177c8493bc7b4b033fa3
AgusBulacio <agusbulacio1803@gmail.com>
2025-10-27 10:19:19 -0300

    hice el componete FormAltaUsuario para que el adminsitrador de el alta a los usuarios con el nombre, apellido, email, telefono, contraseña y rol

--- src/pages/Administrador/UsuariosAdmi.jsx
+++ src/pages/Administrador/UsuariosAdmi.jsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import Tablageneral from '../../components/tablageneral'
+import Boton from '../../components/Boton'
+import Swal from "sweetalert2";
+import FormAltaUsuario from '../Administrador/FormAltaUsuario'
+import { useState, useEffect } from 'react'
 
 const UsuariosAdmi = () => {
-  return (
-    <div>UsuariosAdmi</div>
-  )
-}
+  const columnas = ['Nº','Nombre', 'Apellido', 'Email',"Telefono", 'Rol'];
+   const claves = ["nombre", "apellido", "email", "telefono", "role"];
+  const tipo = "usuarios";
+ const [filas, setFilas] = useState([]);
+  const [mostrarModal, setMostrarModal] = useState(false);
+  const [itemEditar, setItemEditar] = useState(null);
 
-export default UsuariosAdmi
+
+   useEffect(() => {
+    const usuariosGuardadas = localStorage.getItem("usuarios");
+    if (usuariosGuardadas) {
+      setFilas(JSON.parse(usuariosGuardadas));
+    }
+  }, []);
+
+  const abrirModal = () => {
+    setItemEditar(null);
+    setMostrarModal(true);
+  };
+
+  const cerrarModal = () => {
+    setItemEditar(null);
+    setMostrarModal(false);
+  };
+
+
+
+    const editar = (id) => {
+    const usuarios = filas.find((item) => item.id === id);
+    setItemEditar(usuarios);
+    setMostrarModal(true);
+  };
+
+
+    const eliminar = (id) => {
+    const usuarios = filas.find((item) => item.id === id);
+    Swal.fire({
+      title: `¿Eliminar al ${usuarios.nombre}?`,
+      text: "Este cambio no se puede revertir",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, eliminar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const actualizadas = filas.filter((item) => item.id !== id);
+        setFilas(actualizadas);
+        localStorage.setItem(tipo, JSON.stringify(actualizadas));
+        Swal.fire({
+          title: "Eliminado",
+          text: `El ${usuarios.nombre}  fue eliminada correctamente.`,
+          icon: "success",
+        });
+      }
+    });
+  };
+
+  const agegarUsuario = (cita) => {
+    let actualizadas;
+    if (itemEditar) {
+      actualizadas = filas.map((fila) => (fila.id === cita.id ? cita : fila));
+    } else {
+      actualizadas = [...filas, cita];
+    }
+    setFilas(actualizadas);
+    localStorage.setItem(tipo, JSON.stringify(actualizadas));
+    cerrarModal();
+  };
+
+
+  return (
+    <>
+      <Tablageneral
+        columnas={columnas}
+        filas={filas}
+        claves={claves}
+        acciones={(fila) => (
+          <div className="d-flex gap-2 align-items-center justify-content-center">
+            <Boton action="editar" onClick={() => editar(fila.id)} />
+            <Boton action="eliminar" onClick={() => eliminar(fila.id)} />
+          </div>
+        )}
+      />
+      <div className="d-flex justify-content-end mt-3">
+        <Boton action="agregar" onClick={abrirModal} />
+      </div>
+      <FormAltaUsuario
+        show={mostrarModal}
+        onHide={cerrarModal}
+        onGuardar={agegarUsuario}
+        itemEditar={itemEditar}
+      />
+    </>
+  );
+};
+
+
+export default UsuariosAdmi;
