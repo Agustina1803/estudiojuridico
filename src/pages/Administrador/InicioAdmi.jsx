48c79a86e827b0cf0a195813241dfa9e085dfcfd
AgusBulacio <agusbulacio1803@gmail.com>
2025-10-28 16:56:00 -0300

    agrege las card en el inicio de admin para poder contar la cantidad de usuarios

--- src/pages/Administrador/InicioAdmi.jsx
+++ src/pages/Administrador/InicioAdmi.jsx
@@ -1,11 +1,42 @@
-
-
+import { Row, Col, Card } from "react-bootstrap";
 const InicioAdmi = () => {
+  
   return (
     <>
-    
+    <Row>
+      <Col>
+      <Card className="mt-3">
+        <Card.Body>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </Col>
+          <Col>
+      <Card className="mt-3">
+        <Card.Body>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </Col>
+          <Col>
+      <Card className="mt-3">
+        <Card.Body>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </Col>
+    </Row>
     </>
-  )
-}
+  );
+};
 
-export default InicioAdmi
+export default InicioAdmi;
