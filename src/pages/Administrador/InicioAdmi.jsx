82be21a75aa234831850a28f2c5abcb98fc9c1a1
AgusBulacio <agusbulacio1803@gmail.com>
2025-10-28 18:06:22 -0300

    cambiamos la logica para que cuente el total de usuarios con el metodo length

--- src/pages/Administrador/InicioAdmi.jsx
+++ src/pages/Administrador/InicioAdmi.jsx
@@ -8,7 +8,7 @@ const InicioAdmi = () => {
     let abogados = 0;
     let secretaria = 0;
     let admin = 0;
-    let usuariosActivos = 0;
+    let usuariosTotales =usuariosGuardadas.length;
 
     usuariosGuardadas.forEach((usuario) => {
       if (usuario.role === "abog") {
@@ -19,24 +19,25 @@ const InicioAdmi = () => {
         admin++;
       }
     });
-    usuariosActivos = abogados + secretaria + admin;
-    return { abogados, secretaria, admin, usuariosActivos };
+    return { abogados, secretaria, admin, usuariosTotales };
   };
 
-  const { abogados, secretaria, admin, usuariosActivos } = contarUusuario();
+  const { abogados, secretaria, admin, usuariosTotales } = contarUusuario();
   return (
     <>
       <Row className="justify-content-center">
-        <Col md={4}>
+        <Col md={12}>
           <Card className="mt-3 text-center fs-1 bg-primary text-light">
             <Card.Body>
-              <Card.Title>Usuarios activos:</Card.Title>
-              <Card.Text>{usuariosActivos}</Card.Text>
+              <Card.Title>Usuarios totales:</Card.Title>
+              <Card.Text>{usuariosTotales}</Card.Text>
             </Card.Body>
           </Card>
         </Col>
+        </Row>
+        <Row>
         <Col md={4}>
-          <Card className="mt-3 text-center fs-1 bg-success text-light">
+          <Card className="mt-3 text-center fs-1 bg-secondary text-light">
             <Card.Body>
               <Card.Title>Administradores activos:</Card.Title>
               <Card.Text>{admin}</Card.Text>
@@ -44,7 +45,7 @@ const InicioAdmi = () => {
           </Card>
         </Col>
         <Col md={4}>
-          <Card className="mt-3 text-center fs-1 bg-warning text-light">
+          <Card className="mt-3 text-center fs-1 bg-secondary text-light">
             <Card.Body>
               <Card.Title>Abogados activos:</Card.Title>
               <Card.Text>{abogados}</Card.Text>
@@ -52,7 +53,7 @@ const InicioAdmi = () => {
           </Card>
         </Col>
         <Col md={4}>
-          <Card className="mt-3 text-center fs-1 bg-info text-light">
+          <Card className="mt-3 text-center fs-1 bg-secondary text-light">
             <Card.Body>
               <Card.Title>Secretario/a activos/as:</Card.Title>
               <Card.Text>{secretaria}</Card.Text>
