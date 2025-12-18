042e4b22456c01936383e78ea3201b9fe04a9501
AgusBulacio <agusbulacio1803@gmail.com>
2025-12-18 10:54:47 -0300

    agregue las funciones para los metodos de actualizar y deltete de la cita

--- src/helper/cita.Api.js
+++ src/helper/cita.Api.js
@@ -33,3 +33,44 @@ export const crearCita = async (citaNueva) => {
     return null;
   }
 };
+
+export const actualizarCita = async (cita) => {
+  try {
+    const token = localStorage.getItem("token");
+    const respuesta = await fetch(`${urlEstudio}/citas/${cita._id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "x-token": token,
+      },
+      body: JSON.stringify(cita._id),
+    });
+    if (!respuesta.ok) {
+      throw new Error("Error al actualizar la cita");
+    }
+    return await respuesta.json();
+  } catch (error) {
+     console.error(error);
+    return null;
+  }
+};
+
+export const eliminarCita = async (_id) => {
+  try {
+    const token = localStorage.getItem("token");
+    const respuesta = await fetch(`${urlEstudio}/citas/${_id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "x-token": token,
+      },
+    });
+    if (!respuesta.ok) {
+      throw new Error("Error al eliminar la cita");
+    }
+    return true;
+  } catch (error) {
+     console.error(error);
+    return false;
+  }
+};
